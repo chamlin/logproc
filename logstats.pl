1c6657e1c0b42a688bd80fb57cd8694ba36d621e
chamlin <Chris.Hamlin@marklogic.com>
2017-06-30 20:19:07 -0400

    working

--- logstats.pl
+++ logstats.pl
@@ -3,38 +3,75 @@ use strict;
 use Data::Dumper;
 use Carp;
 
+# $state->{config}
+#       ->{config}{config_files}
+#       ->{config}{options}
+#       ->{input}{filename}
+#                {line_number}
+#                {line}
+#       ->{scratch}   for scratch during line match
+#       ->{scratch}{timestamp}
+#       ->{scratch}{text}
+#       ->{scratch}{values}    []
+
 print "ohai\n";
 
-my $state = {};
+my $state = {
+    input => {},
+    config => {
+        config_files => ['standard.config'],
+        data_files => ['short'],
+    },
+};
 
-my $config_file = 'standard.config';
 
-read_configs ($state, [$config_file]);
+read_configs ($state);
 
-my $s = "2015-03-28 00:04:17.959 Info: TaskServer: Starting phony process";
-
-foreach my $matcher (@{$state->{matchers}}) {
-    my $regex = $matcher->{regex};
-    print "trying $regex.\n";
-    my @values = ($s =~ /$regex/);
-    print "values = ", join ('|', @values), ".\n";
-    if (scalar @values) {
-        $state->{values} = \@values;
-        foreach my $action (@{$matcher->{actions}}) {
-            $action->($state);
-        }
+foreach my $file (@{$state->{config}{data_files}}) {
+    print "< $file\n";
+    $state->{input} = {
+        filename => $file,
+        line_number => 0,
+    };
+    open my $fh, '<', $file;
+    while (my $line = <$fh>) {
+        $state->{input}{line} = $line;
+        $state->{input}{line_number}++;
     }
+    close $fh;
 }
 
+check_line ($state);
 
 print "obai\n";
 
 print Dumper $state;
 
+############ subs
+
+sub check_line {
+    my ($state) = @_;
+    my $line = $state->{input}{line};
+    foreach my $matcher (@{$state->{matchers}}) {
+        my $regex = $matcher->{regex};
+        my $text = $state->{scratch}{text};
+        print "trying", Dumper ($matcher), ".\n";
+        my $match_full_line = ($matcher->{match} && $matcher->{match} eq 'line' ? 1 : 0);
+print "mfl: $match_full_line.\n";
+        my @values = (($match_full_line ? $line : $text) =~ /$regex/);
+        print "values = ", join ('|', @values), ".\n";
+        if (scalar @values) {
+            $state->{scratch}{values} = \@values;
+            foreach my $action (@{$matcher->{actions}}) { $action->($state) }
+        }
+    }
+}
+
+
 sub read_configs {
-    my ($state, $config_files) = @_;
+    my ($state) = @_;
     local $/;   # Set input to "slurp" mode.
-    foreach my $config_file (@$config_files) { 
+    foreach my $config_file (@{$state->{config}{config_files}}) { 
         #unless (-f $config_file) { carp "No such config $config_file, skipping.\n"; }
         my ($fh, $dstruct);
         my $error;
